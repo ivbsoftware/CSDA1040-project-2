34f207c90017f60c7a0109168e7fd3c23ddc129a
ibaranov-WS\ibaranov <ivb@outlook.com>
2019-02-12 00:05:02 -0500

    Started some analysis in Shiny app

--- shiny/Complainer/server.R
+++ shiny/Complainer/server.R
@@ -14,7 +14,6 @@ library(tidytext)
 library(Rfast)
 
 
-
 ## Reading in Data 
 numTopics <- 12 # 7, 10 or 30
 topicsFileName <- paste("./lda",numTopics,".Rds", sep = "") 
@@ -25,7 +24,41 @@ full_sorted_issues_list <- readRDS("full_sorted_issues_list.Rds")
 sampleComplaints <- readRDS("sampleComplaints.Rds") 
 sampleComplaintsNum <- nrow(sampleComplaints)
 
-# server logic
+issueTopicsProbMat <- readRDS("issueTopicsProbMat.Rds")
+testThreshold =0.1
+
+## corpus prep function
+prepareCorpus <- function(textArr) {
+  myCorpus <- Corpus(VectorSource(textArr))
+  myCorpus <- tm_map(myCorpus, removePunctuation)
+  myCorpus <- tm_map(myCorpus, removeNumbers)
+  myCorpus <- tm_map(myCorpus, tolower)
+  myStopwords <- c(stopwords(language="en", source="smart"), 
+                   "xx", "xxxx", "xxxxxxxxxxxx", "xxxxxxxx")
+  myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
+  myCorpus <- tm_map(myCorpus, stripWhitespace)
+  myCorpus <- tm_map(myCorpus, stemDocument)
+  
+  return (myCorpus)
+}
+
+## get best topics function
+getBestTopicNums <- function(arr, threshold) {
+  topNums <- sort(arr, index.return=TRUE, decreasing = TRUE)$ix
+  arr <- arr[arr>threshold]
+  return (topNums[1:length(arr)])
+}
+
+distInfo <- function(vec, mat) {
+  retVec <- vector(length = nrow(mat))
+  for (i in 1:nrow(mat)) {
+    retVec[i] <- dist(rbind(vec,mat[i,]))
+  }
+  retVec <- sort(retVec, index.return=TRUE)
+  return (retVec)
+}
+
+## server logic
 shinyServer(function(input, output, session) {
 
   # callback - 'updateRandom' button clicked 
@@ -55,10 +88,14 @@ shinyServer(function(input, output, session) {
   
   # callback - 'recommend' button clicked
   observeEvent(input$analyze, {
-#    output$msg <- renderText({
-#      "Analysis\nAnalysis\nAnalysis"
-#    })
-    
+    text <- input$complaintIn
+    corpus <- prepareCorpus(text)
+    dtm <- DocumentTermMatrix(corpus, control = list(minWordLength = 1))
+    topics <- posterior(train.lda,dtm)
+    issuesFound <- distInfo (topics$topics[1,],issueTopicsProbMat)
+    tops <- getBestTopicNums(topics$topics[1,], testThreshold)
+    print (tops)
+    print (issuesFound)
     showModal(dataModal())
   })
 
